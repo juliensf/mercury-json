c00df2717c379012a25e07e486caa6cd6267dbd4
Julien Fischer <juliensf@gmail.com>
2016-07-31 01:13:58 +1000

    Extend one of the samples.
    samples/pretty.m:
       Extend this example, so that various JSON extensions can be
       controlled via command line options.

--- samples/pretty.m
+++ samples/pretty.m
@@ -1,10 +1,14 @@
 %-----------------------------------------------------------------------------%
 % vim: ft=mercury ts=4 sw=4 et
 %-----------------------------------------------------------------------------%
+% Copyright (C) 2014-2016, Julien Fischer.
+% See the file COPYING for license details.
 %
-% This program reads a JSON value into memory from the standard input stream
-% and, if successful, pretty prints the same value to the standard output
-% stream.
+% Author: Julien Fischer <juliensf@gmail.com>
+%
+% This program reads a JSON value into memory from a specified file (or from
+% the standard input if no file is specified), and, if successful, pretty
+% prints the same value to the standard output stream.
 %
 %-----------------------------------------------------------------------------%
 
@@ -13,7 +17,7 @@
 
 :- import_module io.
 
-:- pred main(io::di, io::uo) is det.
+:- pred main(io::di, io::uo) is cc_multi.
 
 %-----------------------------------------------------------------------------%
 %-----------------------------------------------------------------------------%
@@ -22,30 +26,172 @@
 
 :- import_module json.
 
+:- import_module bool.
+:- import_module char.
+:- import_module exception.
+:- import_module list.
+:- import_module maybe.
+:- import_module getopt.
 :- import_module stream.
+:- import_module string.
 
 %-----------------------------------------------------------------------------%
 
 main(!IO) :-
+    ( try [io(!IO)] (
+        main_2(!IO)
+    )
+    then
+        true
+    catch IO_Error ->
+        io.error_message(IO_Error, Msg),
+        io.stderr_stream(Stderr, !IO),
+        io.print_line(Stderr, Msg, !IO),
+        io.set_exit_status(1, !IO)
+    catch_any Other ->
+        throw(Other)
+    ).
 
-    % Initialise a JSON reader and attach it to the standard input stream.
+:- pred main_2(io::di, io::uo) is det.
+
+main_2(!IO) :-
+    io.command_line_arguments(Args, !IO),
+    OptionOpts = option_ops_multi(
+        short_option,
+        long_option,
+        option_default
+    ),
+    getopt.process_options(OptionOpts, Args, NonOptionArgs, MaybeOptionTable),
+    (
+        MaybeOptionTable = ok(OptionTable),
+        getopt.lookup_bool_option(OptionTable, help, Help),
+        (
+            Help = yes,
+            usage(!IO)
+        ;
+            Help = no,
+            handle_args_and_options(OptionTable, NonOptionArgs, Params,
+                MaybeInputFile, Continue, !IO),
+            (
+                Continue = yes,
+                pretty_print_json(Params, MaybeInputFile, !IO)
+            ;
+                Continue = no
+            )
+        )
+    ;
+        MaybeOptionTable = error(OptionErrorMsg),
+        string.format("error: %s.\n", [s(OptionErrorMsg)], ErrorMsg),
+        print_error(ErrorMsg, !IO)
+    ).
+
+:- pred handle_args_and_options(option_table(option)::in, list(string)::in,
+    reader_params::out, maybe(io.text_input_stream)::out,
+    bool::out, io::di, io::uo) is det.
+
+handle_args_and_options(OptionTable, Args, ReaderParams, MaybeInputFile,
+        !:Continue, !IO) :-
+    !:Continue = yes,
+    (
+        Args = [],
+        MaybeInputFile = no
+    ;
+        Args = [InputFileName],
+        io.open_input(InputFileName, OpenInputResult, !IO),
+        (
+            OpenInputResult = ok(InputFile),
+            MaybeInputFile = yes(InputFile)
+        ;
+            OpenInputResult = error(OpenInputError),
+            io.error_message(OpenInputError, OpenInputErrorMsg),
+            print_error("error: " ++ OpenInputErrorMsg ++ "\n", !IO),
+            MaybeInputFile = no,    % Dummy value.
+            !:Continue = no
+        )
+    ;
+        Args = [_, _ | _],
+        MaybeInputFile = no,
+        ArgErrorMsg = "error: more than one argument.\n",
+        print_error(ArgErrorMsg, !IO),
+        !:Continue = no
+    ),
+    getopt.lookup_bool_option(OptionTable, allow_infinities,
+        AllowInfinitiesOpt),
+    (
+        AllowInfinitiesOpt = no,
+        AllowInfinities = do_not_allow_infinities
+    ;
+        AllowInfinitiesOpt = yes,
+        AllowInfinities = allow_infinities
+    ),
+    getopt.lookup_bool_option(OptionTable, allow_trailing_commas,
+        AllowTrailingCommasOpt),
+    (
+        AllowTrailingCommasOpt = no,
+        AllowTrailingCommas = do_not_allow_trailing_commas
+    ;
+        AllowTrailingCommasOpt = yes,
+        AllowTrailingCommas = allow_trailing_commas
+    ),
+    getopt.lookup_bool_option(OptionTable, allow_comments,
+        AllowCommentsOpt),
+    (
+        AllowCommentsOpt = no,
+        AllowComments = do_not_allow_comments
+    ;
+        AllowCommentsOpt = yes,
+        AllowComments = allow_comments
+    ),
+    getopt.lookup_string_option(OptionTable, repeated_members,
+        RepeatedMembersOpt),
+    ( if string_to_repeated_members(RepeatedMembersOpt,  RepeatedMembers0) then
+        RepeatedMembers = RepeatedMembers0
+    else
+        BadValueMsg = "error: value of '--repeated-members' must be one of" ++
+            " {none,first,last}.\n",
+        print_error(BadValueMsg, !IO),
+        RepeatedMembers = do_not_allow_repeated_members,    % Dummy value.
+        !:Continue = no
+    ),
+    ReaderParams = reader_params(AllowComments, AllowTrailingCommas,
+        RepeatedMembers, AllowInfinities).
+
+:- pred pretty_print_json(json.reader_params::in,
+    maybe(io.text_input_stream)::in, io::di, io::uo) is det.
+
+pretty_print_json(ReaderParams, MaybeInputFile, !IO) :-
+    (
+        MaybeInputFile = yes(InputFile)
+    ;
+        MaybeInputFile = no,
+        io.stdin_stream(InputFile, !IO)
+    ),
+
+    % Initialise a JSON reader and attach it to the input file stream.
     %
-    io.stdin_stream(Stdin, !IO),
-    json.init_reader(Stdin, Reader, !IO),
+    json.init_reader(InputFile, Reader, !IO),
 
     % Ask the reader to get a JSON value from the standard input stream.
     %
     json.read_value(Reader, ValueResult, !IO),
+    (
+        MaybeInputFile = yes(_),
+        io.close_input(InputFile,!IO)
+    ;
+        MaybeInputFile = no
+    ),
     (
         % The reader has returned a JSON value.
         %
         ValueResult = ok(Value),
 
         % Initialise a JSON writer and attach it to the standard output stream.
-        % Set up the writer to pretty print the JSON output.
+        % Set up the writer to pretty print the JSON output.  If the reader
+        % was set up to allow infinities in the JSON, then also allow them in
+        % the writer.
         %
         io.stdout_stream(Stdout, !IO),
-        WriterParams = writer_params(pretty, do_not_allow_infinities),
+        WriterParams = writer_params(pretty, ReaderParams ^ allow_infinities),
         json.init_writer(Stdout, WriterParams, Writer, !IO),
         json.put_value(Writer, Value, !IO)
     ;
@@ -68,6 +214,86 @@ main(!IO) :-
         io.write_string(Stderr, ErrorMsg, !IO),
         io.set_exit_status(1, !IO)
     ).
+
+%-----------------------------------------------------------------------------%
+
+:- type option
+    --->    help
+    ;       allow_infinities
+    ;       allow_trailing_commas
+    ;       allow_comments
+    ;       repeated_members.
+
+:- pred short_option(char::in, option::out) is semidet.
+
+short_option('h', help).
+
+:- pred long_option(string::in, option::out) is semidet.
+
+long_option("help",                  help).
+long_option("allow-infinities",      allow_infinities).
+long_option("allow-trailing-commas", allow_trailing_commas).
+long_option("allow-comments",        allow_comments).
+long_option("repeated-members",      repeated_members).
+
+:- pred option_default(option::out, option_data::out) is multi.
+
+option_default(help, bool(no)).
+option_default(allow_infinities, bool(no)).
+option_default(allow_trailing_commas, bool(no)).
+option_default(allow_comments, bool(no)).
+option_default(repeated_members, string("none")).
+
+:- pred string_to_repeated_members(string::in,
+    allow_repeated_members::out) is semidet.
+
+string_to_repeated_members("none", do_not_allow_repeated_members).
+string_to_repeated_members("first", allow_repeated_members_keep_first).
+string_to_repeated_members("last", allow_repeated_members_keep_last).
+
+%-----------------------------------------------------------------------------%
+
+:- pred print_error(string::in, io::di, io::uo) is det.
+
+print_error(Msg, !IO) :-
+    io.stderr_stream(Stderr, !IO),
+    io.write_string(Stderr, Msg, !IO),
+    io.set_exit_status(1, !IO).
+
+%-----------------------------------------------------------------------------%
+
+:- pred usage(io::di, io::uo) is det.
+
+usage(!IO) :-
+    io.stderr_stream(File, !IO),
+    io.write_string(File, "pretty -- pretty print JSON to standard output.\n", !IO),
+    io.nl(File, !IO),
+    io.write_string(File, "Usage: pretty [<options>] [<arg>]\n", !IO),
+    io.nl(File, !IO),
+    io.write_string(File, "OPTIONS:\n", !IO),
+    write_tabbed_lines(File, [
+        "--help",
+        "\tPrint usage information.",
+        "--repeated-members {none,first,last}",
+        "\tSpecifies whether repeated object members are allowed in the",
+        "\tinput JSON and, if so, which should be included in the output.",
+        "---allow-trailing-commas",
+        "\tAllow trailing commas in arrays and objects in the input JSON.",
+        "--allow-infinities",
+        "\tAllow \"Infinity\" in the input JSON.",
+        "--allow-comments",
+        "\tAllow comments in the input JSON."
+    ], !IO).
+
+:- pred write_tabbed_lines(io.text_output_stream::in, list(string)::in,
+    io::di, io::uo) is det.
+
+write_tabbed_lines(_, [], !IO).
+write_tabbed_lines(File, [Str | Strs], !IO) :-
+    io.write_char(File, '\t', !IO),
+    io.write_string(File, Str, !IO),
+    io.nl(File, !IO),
+    write_tabbed_lines(File, Strs, !IO).
 
 %-----------------------------------------------------------------------------%
 :- end_module pretty.
