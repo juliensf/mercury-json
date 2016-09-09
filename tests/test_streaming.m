b09b54f6eb6792e52062b0af4ad568c08ac928c1
Julien Fischer <juliensf@gmail.com>
2016-09-09 15:15:58 +1000

    Add tests for json.get_value/4.
    tests/test_streaming.m:
    tests/streaming.exp:
        As above.

--- tests/test_streaming.m
+++ tests/test_streaming.m
@@ -15,7 +15,7 @@
 :- import_module io.
 
 % get_generic [DONE]
-% get_value
+% get_value   [DONE]
 % get_object
 % get_array
 
@@ -37,26 +37,36 @@
 test_streaming(File, !IO) :-
     Test1 = "values.json_stream",
     io.format(File, "*** Testing get/4 method (%s) ***\n\n", [s(Test1)], !IO),
-    test_generic_get(File, Test1, !IO),
+    do_stream_test(File, Test1, generic_get_until_eof, !IO),
     io.nl(File, !IO),
 
     Test2 = "bad_values.json_stream",
     io.format(File, "*** Testing get/4 method (%s) ***\n\n", [s(Test2)], !IO),
-    test_generic_get(File, Test2, !IO),
-    io.nl(File, !IO).
+    do_stream_test(File, Test2, generic_get_until_eof, !IO),
+    io.nl(File, !IO),
+
+    Test3 = "values.json_stream",
+    io.format(File, "*** Testing get_value/4 method (%s) ***\n\n", [s(Test3)], !IO),
+    do_stream_test(File, Test3, get_value_until_eof, !IO),
+    io.nl(File, !IO),
+
+    Test4 = "bad_values.json_stream",
+    io.format(File, "*** Testing get_value/4 method (%s) ***\n\n", [s(Test4)], !IO),
+    do_stream_test(File, Test4, get_value_until_eof, !IO).
 
 %-----------------------------------------------------------------------------%
 %-----------------------------------------------------------------------------%
 
-:- pred test_generic_get(io.text_output_stream::in, string::in,
-    io::di, io::uo) is det.
+:- pred do_stream_test(io.text_output_stream::in, string::in,
+    pred(io.text_output_stream, json.reader(io.text_input_stream), io, io)
+    ::in(pred(in, in, di, uo) is det), io::di, io::uo) is det.
 
-test_generic_get(OutputFile, TestFileName, !IO) :-
+do_stream_test(OutputFile, TestFileName, TestPred, !IO) :-
     io.open_input(TestFileName, OpenTestFileResult, !IO),
     (
         OpenTestFileResult = ok(TestFile),
         json.init_reader(TestFile, Reader, !IO),
-        generic_get_until_eof(OutputFile, Reader, !IO),
+        TestPred(OutputFile, Reader, !IO),
         io.close_input(TestFile, !IO)
     ;
         OpenTestFileResult = error(IO_Error),
@@ -64,6 +74,9 @@ test_generic_get(OutputFile, TestFileName, !IO) :-
         io.write_string(OutputFile, Msg, !IO),
         io.nl(OutputFile, !IO)
     ).
+
+%-----------------------------------------------------------------------------%
+
 :- pred generic_get_until_eof(io.text_output_stream::in,
     json.reader(io.text_input_stream)::in, io::di, io::uo) is det.
 
@@ -73,6 +86,25 @@ generic_get_until_eof(File, Reader, !IO) :-
         GetResult = ok(Value),
         json.write_pretty(File, Value, !IO),
         generic_get_until_eof(File, Reader, !IO)
+    ;
+        GetResult = eof
+    ;
+        GetResult = error(Error),
+        Msg = stream.error_message(Error),
+        io.format(File, "error: %s", [s(Msg)], !IO)
+    ).
+
+%-----------------------------------------------------------------------------%
+
+:- pred get_value_until_eof(io.text_output_stream::in,
+    json.reader(io.text_input_stream)::in, io::di, io::uo) is det.
+
+get_value_until_eof(File, Reader, !IO) :-
+    json.get_value(Reader, GetResult, !IO),
+    (
+        GetResult = ok(Value),
+        json.write_pretty(File, Value, !IO),
+        get_value_until_eof(File, Reader, !IO)
     ;
         GetResult = eof
     ;
